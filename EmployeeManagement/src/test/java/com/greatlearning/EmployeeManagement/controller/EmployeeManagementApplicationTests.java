155795df27568c5fe02f54fb80bfb54e269a0f18
Aravind-SY <93550319+Aravind-SY@users.noreply.github.com>
2022-04-09 22:16:59 +0530

    Add files via upload

---
+++ EmployeeManagement/src/test/java/com/greatlearning/EmployeeManagement/controller/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.EmployeeManagement.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
